449a821cbe725baa15ecdf789e9173b333be4d44
Kateryna <koliesnikova.kateryna@gmail.com>
2024-03-20 11:57:08 +0200

    Done

---
+++ js/task-3.js
@@ -0,0 +1,10 @@
+function getElementWidth(content, padding, border) {
+  const contentWidth = parseFloat(content);
+  const paddingWidth = parseFloat(padding);
+  const borderWidth = parseFloat (border);
+  return contentWidth + 2 * paddingWidth + 2 * borderWidth;
+}
+
+console.log(getElementWidth("50px", "8px", "4px")); // 74
+console.log(getElementWidth("60px", "12px", "8.5px")); // 101
+console.log(getElementWidth("200px", "0px", "0px")); // 200
